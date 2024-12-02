3d317908eef6e427398e568dd353fd4976418021
daniel-2-7 <75557992+daniel-2-7@users.noreply.github.com>
2024-12-02 02:25:04 +0000

    Implemented the caching problem
    - Might still need to fix the resubmit of form

--- patient/src/main/java/comp3911/cwk2/AppServer.java
+++ patient/src/main/java/comp3911/cwk2/AppServer.java
@@ -1,6 +1,7 @@
 package comp3911.cwk2;
 
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.session.SessionHandler;
 import org.eclipse.jetty.servlet.ServletHandler;
 import org.eclipse.jetty.util.log.Log;
 import org.eclipse.jetty.util.log.StdErrLog;
@@ -9,13 +10,17 @@ public class AppServer {
   public static void main(String[] args) throws Exception {
     Log.setLog(new StdErrLog());
 
-    ServletHandler handler = new ServletHandler();
-    handler.addServletWithMapping(AppServlet.class, "/*");
+    ServletHandler servletHandler = new ServletHandler();
+    servletHandler.addServletWithMapping(AppServlet.class, "/*");
+
+    SessionHandler sessionHandler = new SessionHandler();
+    sessionHandler.setHandler(servletHandler);
 
     Server server = new Server(8080);
-    server.setHandler(handler);
+    server.setHandler(sessionHandler);
 
     server.start();
     server.join();
   }
 }
+
